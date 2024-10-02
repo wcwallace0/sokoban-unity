e6aed9db8b87d340e3ee603943298b6676310832
Will Wallace <wwallac1@trinity.edu>
2024-10-01 23:54:22 -0500

    Visual and implementation tweaks to player movement/animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     public Transform target; // Position that the player moves towards
 
     public LayerMask stopsMovement;
+    public LayerMask moveable;
 
     // Animation parameter - Facing
     // 0 - North
@@ -25,23 +26,30 @@ public class PlayerMovement : MonoBehaviour {
             if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) 
             {
                 Vector3 moveBy = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                if(!Physics2D.OverlapCircle(target.position + moveBy, 0.2f, stopsMovement)) 
-                {
-                    target.position += moveBy;
-                    anim.SetFloat("Facing", -1 * moveBy.x + 2);
-                    anim.SetBool("isWalking", true);
-                }
+                anim.SetFloat("Facing", -1 * moveBy.x + 2);
+                CheckMovement(moveBy);
             } 
             else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) 
             {
                 Vector3 moveBy = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                if(!Physics2D.OverlapCircle(target.position + moveBy, 0.2f, stopsMovement)) 
-                {
-                    target.position += moveBy;
-                    anim.SetFloat("Facing", -1 * moveBy.y + 1);
-                    anim.SetBool("isWalking", true);
-                }
+                anim.SetFloat("Facing", -1 * moveBy.y + 1);
+                CheckMovement(moveBy);
             }
+        }
+    }
+
+    // Given a Vector3 indicating where the player wishes to move,
+    // check if that new position is walkable.
+    // If not, or if the new position contains a box, move the player/boxes accordingly
+    void CheckMovement(Vector3 moveBy) {
+        if(Physics2D.OverlapCircle(target.position + moveBy, 0.2f, moveable)) 
+        {
+            Debug.Log("push box");
+        }
+        else if(!Physics2D.OverlapCircle(target.position + moveBy, 0.2f, stopsMovement)) 
+        {
+            target.position += moveBy;
+            anim.SetBool("isWalking", true);
         }
     }
 }
