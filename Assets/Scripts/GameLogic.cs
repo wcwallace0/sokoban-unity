8a921a01aa5959fa0a8e355b537a9e4d0300613b
Will Wallace <wwallac1@trinity.edu>
2024-10-02 19:43:03 -0500

    Implemented restart option

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour
 {
     public Button[] buttons;
+    private bool won;
+
+    private void Update() {
+        if(!won && Input.GetKeyDown(KeyCode.R)) {
+            // Reload Scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 
     public void CheckWin() {
         foreach(Button button in buttons) {
             if(!button.pressed) {
+                won = false;
                 return;
             }
         }
@@ -16,5 +26,6 @@ public class GameLogic : MonoBehaviour
         // All buttons are pressed, win condition is passed
         // TODO
         Debug.Log("WIN");
+        won = true;
     }
 }
